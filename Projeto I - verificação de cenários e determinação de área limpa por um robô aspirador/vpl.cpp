31ddc678b23642686be2a61ff3e221faf4ca5e40
oasjs <oasjatoba1@gmail.com>
2023-05-12 17:37:51 -0300

    Refatora, corrige, otimiza e traduz código

--- Projeto I - verificação de cenários e determinação de área limpa por um robô aspirador/vpl.cpp
+++ Projeto I - verificação de cenários e determinação de área limpa por um robô aspirador/vpl.cpp
@@ -5,192 +5,248 @@
 #include <queue>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
-
-std::string validaXml(const std::string& filename) {
+// Retorna o conteúdo do XML, caso ele seja válido. Se não, imprime "erro" e
+// retorna uma string vazia.
+std::string validateXML(const std::string& filename) {
     // Leitura do arquivo e inicilização da string do XML
     std::ifstream xmlFile(filename);
     if (!xmlFile.is_open()) {
         std::cout << "erro" << std::endl;
+        return "";
     }
     std::string xmlContent((std::istreambuf_iterator<char>(xmlFile)), std::istreambuf_iterator<char>());
     xmlFile.close();
 
-    std::stack<std::string> pilhaTags; // pilha para armazenar as tags
-    bool erro = false; // flag para indicar erro de validação
-    for (int i = 0; i < xmlContent.length() && !erro; i++) {
+    std::stack<std::string> tagStack; // pilha para armazenar as tags
+    for (int i = 0; i < xmlContent.length(); i++) {
         if (xmlContent[i] == '<') {
             if (xmlContent[i + 1] == '/') { // tag de fechamento
                 i += 2;
-                std::string tagFechamento = "";
-                while (xmlContent[i] != '>' && tagFechamento.length() < 20) {
-                    tagFechamento += xmlContent[i];
+                std::string closingTag = "";
+                while (xmlContent[i] != '>' && closingTag.length() < 20) {
+                    closingTag += xmlContent[i];
                     i++;
                 }
-                if (pilhaTags.empty()) { // erro de validação: tag de fechamento sem correspondente de abertura
+                if (tagStack.empty()) { // erro de validação: tag de fechamento sem correspondente de abertura
                     std::cout << "erro" << std::endl;
-                    erro = true;
+                    return "";
                 } else { // tag de fechamento válida
                     // Se a ultima aberta for diferente da que esta fechando
-                    if (pilhaTags.top() != tagFechamento) {
+                    if (tagStack.top() != closingTag) {
                         std::cout << "erro" << std::endl;
-                        erro = true;
+                        return "";
                     } else {
-                        pilhaTags.pop();
+                        tagStack.pop();
                     }
                 }
             } else { // tag de abertura
                 i++;
-                std::string tagAbertura = "";
-                while (xmlContent[i] != '>' && tagAbertura.length() < 20) {
-                    tagAbertura += xmlContent[i];
+                std::string openingTag = "";
+                while (xmlContent[i] != '>' && openingTag.length() < 20) {
+                    openingTag += xmlContent[i];
                     i++;
                 }
-                pilhaTags.push(tagAbertura);
+                tagStack.push(openingTag);
             }
         }
     }
-    if (!pilhaTags.empty()) { // erro de validação: tag de abertura sem correspondente de fechamento
+    if (!tagStack.empty()) { // erro de validação: tag de abertura sem correspondente de fechamento
         std::cout << "erro" << std::endl;
-        erro = true;
+        return "";
         }
     return xmlContent;
 }
 
-std::pair<int, int> extraiDimensoes(const std::string& xmlContent) {
-    int altura = 0, largura = 0;
-    size_t pos = xmlContent.find("<dimensoes>");
+
+// Retorna um par int<x, y> contendo as dimensões da matriz do cenário provido.
+std::pair<int, int> getMatrixDimensions(const std::string& xmlContent, std::size_t casePos) {
+    int height = 0, width = 0;
+    size_t pos = xmlContent.find("<dimensoes>", casePos);
     if (pos != std::string::npos) {
         pos = xmlContent.find("<altura>", pos);
         if (pos != std::string::npos) {
             size_t endPos = xmlContent.find("</altura>", pos);
             if (endPos != std::string::npos) {
-                std::string alturaStr = xmlContent.substr(pos + 8, endPos - pos - 8);
-                altura = std::stoi(alturaStr);
+                std::string heightStr = xmlContent.substr(pos + 8, endPos - pos - 8);
+                height = std::stoi(heightStr);
             }
         }
         pos = xmlContent.find("<largura>", pos);
         if (pos != std::string::npos) {
             size_t endPos = xmlContent.find("</largura>", pos);
             if (endPos != std::string::npos) {
-                std::string larguraStr = xmlContent.substr(pos + 9, endPos - pos - 9);
-                largura = std::stoi(larguraStr);
+                std::string widthStr = xmlContent.substr(pos + 9, endPos - pos - 9);
+                width = std::stoi(widthStr);
             }
         }
     }
-    return std::make_pair(altura, largura);
+    return std::make_pair(height, width);
 }
 
-std::pair<int, int> extraiPosicaoInicial(const std::string& xmlContent) {
-    int alturaRobo = 0, larguraRobo = 0;
-    size_t pos = xmlContent.find("<robo>");
+
+// Extrai a posição inicial (x, y) do robo, invertendo a relação que o XML
+// considera: (x) como deslocamento vertical e (y) como horizontal.
+//
+// Retorna um par int<x, y> contendo a posição inicial do robô dada a posição
+// do cenário no XML.
+std::pair<int, int> getBotPosition(const std::string& xmlContent, std::size_t casePos) {
+    int bot_x = 0, bot_y = 0;
+    size_t pos = xmlContent.find("<robo>", casePos);
     if (pos != std::string::npos) {
         pos = xmlContent.find("<x>", pos);
         if (pos != std::string::npos) {
             size_t endPos = xmlContent.find("</x>", pos);
             if (endPos != std::string::npos) {
-                std::string alturaStr = xmlContent.substr(pos + 3, endPos - pos - 3);
-                alturaRobo = std::stoi(alturaStr);
+                std::string yStr = xmlContent.substr(pos + 3, endPos - pos - 3);
+                bot_y = std::stoi(yStr);
             }
         }
         pos = xmlContent.find("<y>", pos);
         if (pos != std::string::npos) {
             size_t endPos = xmlContent.find("</y>", pos);
             if (endPos != std::string::npos) {
-                std::string larguraStr = xmlContent.substr(pos + 3, endPos - pos - 3);
-                larguraRobo = std::stoi(larguraStr);
+                std::string xStr = xmlContent.substr(pos + 3, endPos - pos - 3);
+                bot_x = std::stoi(xStr);
             }
         }
     }
-    return std::make_pair(alturaRobo, larguraRobo);
+    return std::make_pair(bot_x, bot_y);
 }
 
-std::string extraiNomeDoCenario(const std::string& xmlContent) {
-    std::string nomeDoCenario;
-    size_t pos = xmlContent.find("<cenario>");
+
+// Retorna o nome do cenário dada sua posição no XML.
+std::string getCaseName(const std::string& xmlContent, std::size_t casePos) {
+    std::string caseName;
+    size_t pos = xmlContent.find("<cenario>", casePos);
     if (pos != std::string::npos) {
         pos = xmlContent.find("<nome>", pos);
         if (pos != std::string::npos) {
             size_t endPos = xmlContent.find("</nome>", pos);
             if (endPos != std::string::npos) {
-                nomeDoCenario = xmlContent.substr(pos + 6, endPos - pos - 6);
+                caseName = xmlContent.substr(pos + 6, endPos - pos - 6);
             }
         }
     }
-    return nomeDoCenario;
+    return caseName;
 }
 
-int** matrizZerada(std::pair<int, int> dimensoes) {
-    int altura = dimensoes.first;
-    int largura = dimensoes.second;
 
-    int** matriz = new int*[altura];
-    for (int i = 0; i < altura; i++) {
-        matriz[i] = new int[largura];
-        for (int j = 0; j < largura; j++) {
-            matriz[i][j] = 0;
+// Retorna uma matriz de inteiros zerada conforme as dimensões providas.
+int** getClearMatrix(std::pair<int, int> dimensions) {
+    int height = dimensions.first;
+    int width = dimensions.second;
+
+    int** matrix = new int*[height];
+    for (int i = 0; i < height; i++) {
+        matrix[i] = new int[width];
+        for (int j = 0; j < width; j++) {
+            matrix[i][j] = 0;
         }
     }
-    return matriz;
+    return matrix;
 }
 
-int** matrizOriginal(std::pair<int, int> dimensoes, const std::string& xmlContent) {
-    int altura = dimensoes.first;
-    int largura = dimensoes.second;
-   
-    int** matriz = new int*[altura];
-    size_t pos = xmlContent.find("<matriz>") + 8;
+
+int** getCaseMatrix(const std::string& xmlContent, std::pair<int, int> dimensions, std::size_t casePos) {
+    int height = dimensions.first;
+    int width = dimensions.second;
+    int** matrix = new int*[height];
+
+    size_t pos = xmlContent.find("<matriz>", casePos);
+    std::string matrixStr;
     if (pos != std::string::npos) {
-        for (int i = 0; i < altura; i++) {
-            for (int j = 0; j < largura; j++) {
-                matriz[i][j] = xmlContent[pos * i + j];
-                std::cout << matriz[i][j] << "\n";
-            }
+        std::size_t endPos = xmlContent.find("</matriz>");
+        if (endPos != std::string::npos) {
+            matrixStr = xmlContent.substr(pos+9, endPos-(pos+9));
         }
     }
-    return matriz;
+    matrixStr.erase(std::remove(matrixStr.begin(), matrixStr.end(), '\n'),
+                    matrixStr.end());
+
+    for (int i = 0; i < height; i++) {
+        matrix[i] = new int[width];
+        for (int j = 0; j < width; j++) {
+            char cell = matrixStr[i * width + j];
+            if (cell == '0' || cell == '1') {
+                int intCell = cell - '0';
+                matrix[i][j] = intCell;
+            }
+        }
+        std::cout << "\n";
+    }
+    return matrix;
 }
 
-int main() {
-    char xmlfilename[100];
-    std::cin >> xmlfilename;
-    std::string xmlContent = validaXml(xmlfilename);
+// Funcao auxiliar que retorna o número de cenários de um XML.
+int getNumOfCases(std::string xmlContent) {
+    int count = 0;
+    std::size_t pos = 0;
+    std::string s = xmlContent;
+    std::string target = "<cenario>";
+    while ((pos = s.find(target, pos )) != std::string::npos) {
+            ++count;
+            pos += target.length();
+    }
+    return count;
+}
 
-    // std::pair<int, int> dimensoes = extraiDimensoes(xmlContent);
-    // int altura = dimensoes.first;
-    // int largura = dimensoes.second;
 
-    // int** matrizOriginalZerada = matrizZerada(dimensoes);
-   
+// Retorna um vetor contendo a posição inicial de cada tag cenario dado um XML.
+std::size_t* getCasesPositions(std::string xmlContent, int numOfCases) {
+    std::size_t* casesPos = new std::size_t[numOfCases];
 
-    // std::pair<int, int> posicaoInicial = extraiPosicaoInicial(xmlContent);
-    // int alturaInicial = posicaoInicial.first;
-    // int larguraInicial = posicaoInicial.second;
+    std::string s = xmlContent;
+    std::size_t offset = 0;
 
-    // std::string nomeCenario = extraiNomeDoCenario(xmlContent);
+    for (int i = 0; i < numOfCases; i++) {
+        std::size_t startPos = s.find("<cenario>", offset);
+        std::size_t endPos = s.find("</cenario>", offset);
+        offset = endPos + 10;
+
+        casesPos[i] = startPos;
+    }
+    return casesPos;
+}
+
+
+// Imprime o resultado da rotina do robô.
+void printResult(std::string caseName, int numOfClearedCells);
+
+
+// Retorna o número de células limpas pelo robô.
+int runBot(int** clearMatrix, int** caseMatrix, std::pair<int, int> matrixDimensions, std::pair<int, int> botPos);
+
+
+// Inicializa a rotina de limpeza do robô.
+void initBotRoutine(std::string xmlContent, std::size_t casePos) {
+    
+    std::pair<int, int> dimensions = getMatrixDimensions(xmlContent, casePos);
+    int **clearMatrix = getClearMatrix(dimensions);
+    int **caseMatrix = getCaseMatrix(xmlContent, dimensions, casePos);
+    std::pair<int, int> botPos = getBotPosition(xmlContent, casePos);
+
+    int numOfClearedCells = runBot(clearMatrix, caseMatrix, dimensions, botPos);
+
+    std::string caseName = getCaseName(xmlContent, casePos);
+    void printResult(std::string caseName, int clearedCells);
+}
+
+int main(int argc, char** argv) {
+
+    char* xmlfilename = argv[1];
+    std::string xmlContent = validateXML(xmlfilename);
+
+    int numOfCases = getNumOfCases(xmlContent);
+
+    std::size_t* casePos = getCasesPositions(xmlContent, numOfCases);
+    initBotRoutine(xmlContent, casePos[0]);
+
+    // for (int i = 0; i < numOfCases; i++) {
+    //     initBotRoutine(xmlContent, casePos[i]);
+    // }
 
     return 0;
 
 }
-
-// -----------------------------------------------------------------------------------------
-
-// TO-DO:
-
-// 1. Achar uma forma de prevenir que o programa identifique quando a matriz no XML é inválida (exemplo das imagens)
-// 2. Criar uma função semelhante a extraiDimensoes, mas que extraia a matriz do XML em forma de string (filtrar
-// apenas os 0s e 1s)
-// 3. Criar uma função semelhante a extraiDimensoes, mas que extraia a posição inicial (x,y) do robô OK
-// 4. Criar uma função semelhante a extraiDimensoes, mas que extraia o nome da matriz (exemplo: "cenario-01"), OK
-// para que o programa possa printar o nome da matriz na saída e também para que o programa salve a maior região
-// conexa entre todos os cenários daquele cenario especifico (exemplo: "cenario-01 25")
-// Exemplo de saída do programa:
-//
-// cenario-01 25
-// cenario-02 107
-// cenario-03 28
-// cenario-04 0
-// cenario-05 22
-// cenario-06 174
-
-// -----------------------------------------------------------------------------------------
